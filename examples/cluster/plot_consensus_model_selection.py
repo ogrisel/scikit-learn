f4eed7a428008210a79eba7e7c9ee87b03347cad
Olivier Grisel <olivier.grisel@ensta.org>
2011-09-16 16:50:55 +0200

    msimpler code, print progress

--- examples/cluster/plot_consensus_model_selection.py
+++ examples/cluster/plot_consensus_model_selection.py
@@ -5,6 +5,9 @@ Finding the best number of clusters using a stability criterion
 
 TODO: write me and references to von Luxburg and Vinh.
 """
+# Author: Olivier Grisel <olivier.grisel@ensta.org>
+# License: Simplified BSD
+from time import time
 
 import numpy as np
 import pylab as pl
@@ -16,41 +19,43 @@ from sklearn.neighbors import kneighbors_graph
 
 digits = load_digits()
 
-n_samples = digits.data.shape[0]
 n_runs = 10
-n_clusters_range = np.arange(2, 21)
-n_samples_split = n_samples / 3
+n_clusters_range = np.arange(2, 20)
 
 scores = np.zeros((n_clusters_range.shape[0], n_runs))
-X = digits.data.copy()[:100]
+X = digits.data.copy()
 rng = np.random.RandomState(42)
 
 for j in range(n_runs):
+    t0 = time()
+
     # sample two overlapping sub-datasets
     rng.shuffle(X)
-    X_a = X[:n_samples_split]
-    X_b = X[n_samples_split:2 * n_samples_split]
-    X_c = X[2 * n_samples_split:3 * n_samples_split]
+    X_a, X_b, X_c = np.array_split(X, 3)
+    n_common = X_a.shape[0]
 
     X_ab = np.vstack((X_a, X_b))
     S_ab = kneighbors_graph(X_ab, 10)
+
     X_ac = np.vstack((X_a, X_c))
     S_ac = kneighbors_graph(X_ac, 10)
 
     for i, k in enumerate(n_clusters_range):
         # find a clustering for each sub-sample
-        model_ab = SpectralClustering(k=k, mode='arpack', n_init=1,
+        model_ab = SpectralClustering(k=k, mode='arpack', n_init=10,
                                       random_state=rng).fit(S_ab)
-        model_ac = SpectralClustering(k=k, mode='arpack', n_init=1,
+        model_ac = SpectralClustering(k=k, mode='arpack', n_init=10,
                                       random_state=rng).fit(S_ac)
 
         # extract the label assignment on the overlap
-        labels_ab = model_ab.labels_[:n_samples_split]
-        labels_ac = model_ac.labels_[:n_samples_split]
+        labels_ab = model_ab.labels_[:n_common]
+        labels_ac = model_ac.labels_[:n_common]
 
         # measure the stability of k as the agreement of the clusterings on the
         # overlap
         scores[i, j] = adjusted_rand_score(labels_ab, labels_ac)
+
+    print "Run %d/%d done in %0.2fs" % (j + 1, n_runs, time() - t0)
 
 pl.errorbar(n_clusters_range, scores.mean(axis=1), scores.std(axis=1))
 pl.title("Consensus model selection for Spectral Clustering\n"
