4f7d51d3c87e83becd4defb0c6c31b2a354b206a
Issam Laradji <issam.laradji@gmail.com>
2014-01-20 09:40:13 +0300

    squashed last 40 commits

--- doc/modules/classes.rst
+++ doc/modules/classes.rst
@@ -946,6 +946,8 @@ Pairwise metrics
    :template: class.rst
 
    neural_network.BernoulliRBM
+   neural_network.MultilayerPerceptronClassifier
+   neural_network.MultilayerPerceptronRegressor
 
 
 .. _cross_decomposition_ref:
--- sklearn/neural_network/__init__.py
+++ sklearn/neural_network/__init__.py
@@ -1,8 +1,14 @@
-
 """
 The :mod:`sklearn.neural_network` module includes models based on neural
 networks.
 """
 
+# Licence: BSD 3 clause
+
 from .rbm import BernoulliRBM
-from .mlp import MultilayerPerceptronClassifier
+from .multilayer_perceptron import MultilayerPerceptronClassifier
+from .multilayer_perceptron import MultilayerPerceptronRegressor
+
+__all__ = ["BernoulliRBM",
+           "MultilayerPerceptronClassifier",
+           "MultilayerPerceptronRegressor"]
