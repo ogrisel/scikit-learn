d544f70c34258e45646bf210d75683722be31e2c
Olivier Grisel <olivier.grisel@ensta.org>
2016-02-10 13:47:39 +0100

    matplotlib forces an old version of numpy and a segfault

--- continuous_integration/circle/build_doc.sh
+++ continuous_integration/circle/build_doc.sh
@@ -38,7 +38,7 @@ popd
 # Configure the conda environment and put it in the path using the
 # provided versions
 conda create -n testenv --yes --quiet python numpy scipy \
-  cython nose coverage matplotlib=1.4.0 sphinx pillow
+  cython nose coverage matplotlib sphinx pillow
 source /home/ubuntu/miniconda/envs/testenv/bin/activate testenv
 
 # Build and install scikit-learn in dev mode
