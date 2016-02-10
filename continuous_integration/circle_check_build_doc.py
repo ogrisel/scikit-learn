0d9ecd4cae7a0213bf77d3f8f03d1642d236a9f6
Olivier Grisel <olivier.grisel@ensta.org>
2016-02-10 09:53:04 +0100

    WIP more experimental work on the circle_check_build_doc.py script

--- continuous_integration/circle_check_build_doc.py
+++ continuous_integration/circle_check_build_doc.py
@@ -1,7 +1,8 @@
 """Check whether we or not we should build the documentation
 
-If the last commit message has a "[skip: doc]" marker, do not build
-the doc.
+If the last commit message has a "[doc: skip]" marker, do not build
+the doc. On the contrary if a "[doc: force]" marker is found, build the doc
+instead of relying on the subsequent rules.
 
 We always build the documentation for jobs that are not related to a specific
 PR (e.g. a merge to master or a maintenance branch).
@@ -9,34 +10,55 @@ PR (e.g. a merge to master or a maintenance branch).
 If this is a PR, check that if there are some files in this PR that are under
 the "doc/" or "examples/" folders, otherwise skip.
 
+If the introspection of the current commit fails for any reason, the default
+behavior is to build the documentation.
+
 """
 import sys
 import os
-from subprocess import check_output
+from subprocess import check_output, CalledProcessError
 
+
+def exit(msg="", skip=False):
+    print("%s: %s" % ("SKIP" if skip else "BUILD", msg))
+    sys.exit(0)
+
+# Introspect the message for the commit that triggered the build
+commit = os.environ.get('CIRCLE_SHA1')
+if not commit:
+    exit("undefined CIRCLE_SHA1 variable")
+try:
+    commit_msg = check_output("git log --format=%B -n 1".split() + [commit])
+    commit_msg = commit_msg.decode('utf-8')
+except CalledProcessError:
+    exit("failed to introspect commit message for %s" % commit)
+
+if "[doc: skip]" in commit_msg:
+    exit("[doc: skip] tag found", skip=True)
+elif "[doc: force]" in commit_msg:
+    exit("[force: doc] tag found")
+
+# Check whether this commit is part of a pull request or not
 pr_url = os.environ.get('CI_PULL_REQUEST')
 if not pr_url:
     # The documentation should be always built when executed from one of the
     # main branches
-    print("OK: not a pull request")
-    sys.exit(0)
+    exit("not a pull request")
 
-commit = os.environ.get('CIRCLE_SHA1')
-if not commit:
-    print("SKIP: undefined CIRCLE_SHA1 variable")
-    sys.exit(0)
-
+# Introspect the list of files changed by all the commits in this PR.
 # Hardcode the assumption that this is a PR to origin/master of this repo
 # as apparently there is way to reliably get the target of a PR with circle
 # ci
-git_range = "origin/master...%s" % commit
-
-filenames = check_output("git diff --name-only".split() + [git_range])
+git_range = "origin/master..%s" % commit
+try:
+    filenames = check_output("git diff --name-only".split() + [git_range])
+except CalledProcessError:
+    exit("git introspection failed.")
 filenames = filenames.decode('utf-8').split()
 for filename in filenames:
     if filename.startswith(u'doc/') or filename.startswith(u'examples/'):
-        print("OK: detected doc impacting file at %s" % filename)
-        sys.exit(0)
+        exit("detected doc impacting file at %s" % filename)
 
-print("SKIP: no doc impacting files detected:")
-print(u"\n".join(filenames))
+# This PR does not seem to have any documentation related file changed.
+msg = "no doc impacting files detected:\n" + u"\n".join(filenames)
+exit(msg, skip=True)
